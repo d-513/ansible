11e8682a60cd208a79915c3473565aae8594af04
dada513 <dada513@protonmail.com>
2022-09-03 11:00:01 +0200

    Hedgedoc

--- roles/postgres/files/init.sh
+++ roles/postgres/files/init.sh
@@ -3,3 +3,4 @@ set -e
 
 createdb --encoding=UTF8 --owner=postgres --template=template0 nextcloud
 createdb --encoding=UTF8 --owner=postgres --template=template0 authelia
+createdb --encoding=UTF8 --owner=postgres --template=template0 hedgedoc
