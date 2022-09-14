07afcdba834e5d0f7ae99845fe9606a51a0321d0
dada513 <dada513@protonmail.com>
2022-09-14 19:49:48 +0200

    y

--- roles/postgres/files/init.sh
+++ roles/postgres/files/init.sh
@@ -6,3 +6,5 @@ createdb --encoding=UTF8 --owner=postgres --template=template0 authelia
 createdb --encoding=UTF8 --owner=postgres --template=template0 hedgedoc
 createdb --encoding=UTF8 --owner=postgres --template=template0 synapse
 createdb --encoding=UTF8 --owner=postgres --template=template0 mautrix-whatsapp
+createdb --encoding=UTF8 --owner=postgres --template=template0 mautrix-discord
+createdb --encoding=UTF8 --owner=postgres --template=template0 mautrix-facebook
